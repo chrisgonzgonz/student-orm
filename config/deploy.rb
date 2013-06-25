2511ba3edc6bc647e5fc2c16df0d32d2759531d0
Chris Gonzales <gonzales.chris.a@gmail.com>
2013-06-25 15:14:05 -0400

    post capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,23 @@
 require 'bundler/capistrano' #for bundler support
 
 set :application, "students"
-set :repository,  "set your repository location here"
+set :repository,  "git@github.com:chrisgonzgonz/student-orm.git"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :user, 'chrisgonzgonz'
+set :deploy_to, "/home/#{ user }/#{ application }"
+set :use_sudo, false
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :git
+
+default_run_options[:pty] = true
+
+
+role :web, "192.241.133.93"                          # Your HTTP server, Apache/etc
+role :app, "192.241.133.93"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -17,11 +25,11 @@ role :db,  "your slave db-server here"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
- If you are using Passenger mod_rails uncomment this:
- namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
  end
+end
